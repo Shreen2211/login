0132e10ec87a739fd289f139e9ca77da675c0f57
Shreen Ahmed <shreenahmed2211@gmail.com>
2023-10-06 13:18:53 +0300

    first project flutter

---
+++ android/app/src/main/java/com/shreen/login/MainActivity.java
@@ -0,0 +1,6 @@
+package com.shreen.login;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/componant/bottom_login.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:login/Screens/login.dart';
+
+class bottomLogin extends StatelessWidget {
+  String ? page;
+  Widget  name;
+
+   bottomLogin({required this.name,required this.page,super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(style: ElevatedButton.styleFrom(
+      primary: Colors.white,
+      elevation: 0,// background
+    ),
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => name,
+              ));
+        },
+        child: Text('$page',style: TextStyle(color: Colors.lightBlue),),
+    );
+  }
+}
